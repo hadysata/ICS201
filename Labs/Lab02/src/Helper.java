bef7ecd55be4ba103419caadd446ed33e14e4308
Hady <hadysata@gmail.com>
2020-09-07 12:27:06 +0300

    Lab02

---
+++ Labs/Lab02/src/Helper.java
@@ -0,0 +1,13 @@
+public class Helper {
+
+    public static void main(String[] args) {
+
+        MyPoint testPoint1 = new MyPoint();
+        MyPoint testPoint2 = new MyPoint(10, 30.5);
+
+        System.out.printf("The distance between (%.2f,%.2f) and (%.2f,%.2f) is %.2f %n", testPoint1.getX(),
+                testPoint1.getY(), testPoint2.getX(), testPoint2.getY(), testPoint1.distance(testPoint2));
+
+    }
+
+}
---
+++ Labs/Lab02/src/MyPoint.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+
+public class MyPoint {
+
+    private double x, y;
+
+    public MyPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public MyPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double distance(double x, double y) {
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    double distance(MyPoint point) {
+        return distance(point.getX(), point.getY());
+    }
+
+    static double distance(MyPoint point1, MyPoint point2) {
+        return point1.distance(point2);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
