027ce1d35f9fb0ee065c65559573a724ed1a0639
Hady <hadysata@gmail.com>
2020-10-28 23:59:16 +0300

    ref

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-      java.util.Date x = new java.util.Date();
-      java.util.Date y = (java.util.Date)x.clone();
-      System.out.println(x = y);
-    }
-  }
